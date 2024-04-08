03e601d2f14947f6f7af10fe4813045dbe730ece
Himanshu Kumar <himanshukr033@gmail.com>
2024-04-08 22:54:21 +0530

    Complete version

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,6 +24,7 @@ export default function Home() {
       style={{
         height: "100%",
         position: "relative",
+        overflowX: "hidden",
         display: "flex",
         flexDirection: "column",
         justifyContent: "space-evenly",
